63e0e884a31f302be27676c3c1c21e83774fe902
h1trx <guillermoivansalgadosandoval@gmail.com>
2026-04-10 14:13:12 -0500

    fix: db url

--- config/db.py
+++ config/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base
 
 #engine = create_engine("postgresql://postgres+asyncpg:FkHecP0E7TzPllCb@db.yyuqbvoevfpbswnyxpoo.supabase.co:5432/postgres")
 
-engine = create_async_engine("postgresql+asyncpg://postgres:2yBXiAom7L4wfbxE@db.yyuqbvoevfpbswnyxpoo.supabase.co:5432/postgres")
+engine = create_async_engine("postgresql+asyncpg://postgress:pOakUYRGQYlMWx1H5JTXrambfB22sCsT@dpg-d6tiousr85hc73f9vdjg-a.oregon-postgres.render.com/social_db_bc6g")
 
 Base = declarative_base()
 metadata = Base.metadata
